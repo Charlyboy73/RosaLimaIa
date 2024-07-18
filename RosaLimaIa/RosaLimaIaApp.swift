845e5eb55c935b2e55eab1fa81685628952904ee
CharlyboyGit <estradamirandacarlos@gmail.com>
2024-07-18 08:30:07 -0600

    Initial Commit

---
+++ RosaLimaIa/RosaLimaIaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RosaLimaIaApp.swift
+//  RosaLimaIa
+//
+//  Created by Carlos Estrada on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct RosaLimaIaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
